049c0cee437e4c0ef8780c42a8745b0db1685d9f
alausaJNR <rasheedhabib400@gmail.com>
2022-10-18 16:32:39 -0700

    header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints _putchar and a new line
- * Return: Always 0
+ * Return: 0
  */
 
 int main(void)
 {
-	_putchar("_");
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
