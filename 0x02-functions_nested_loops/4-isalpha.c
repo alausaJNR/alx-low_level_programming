bc04e8b13e118b21c32397caa45ded6de44cac80
alausaJNR <rasheedhabib400@gmail.com>
2022-10-18 17:16:40 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,14 @@
  */
 int _isalpha(int c)
 {
-((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? return(1) : return(0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 
