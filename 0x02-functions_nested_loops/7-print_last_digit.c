d0232b841bd4f5c4cb3b1e3bd174f7fccb4cec30
alausaJNR <rasheedhabib400@gmail.com>
2022-10-18 17:54:30 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,15 @@
  */
 int print_last_digit(int a)
 {
-	int last_digit;
+	int last_digit, p;
 
 	last_digit = a %  10;
 	if (last_digit < 0)
 	{
-		last_digit = last_digit * -1
+		_putchar(p = last_digit * -1)
 	}
-	_putchar(last_digit + '0');
-	return (last_digit);
+	else
+		return (last_digit);
 }
 
 
