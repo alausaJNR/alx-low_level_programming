75d28108f4c010a7ceffb800f05be7887e9967eb
alausaJNR <rasheedhabib400@gmail.com>
2022-10-20 15:13:50 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -41,13 +41,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else if (c > b)
 	{
 		largest = c;
 	}
 	else
 	{
-		largest = printf("No single value is the highest");
+		largest = b;
 	}
 	return (largest);
 }
