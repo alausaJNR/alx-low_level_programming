76f806cee0f26afe39eac4a808d838ea83695ac6
alausaJNR <rasheedhabib400@gmail.com>
2022-10-23 12:18:00 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("%s", fb);
+			printf("%s ", fb);
 		else if (i % 5 == 0)
-			printf("%s", b);
+			printf("%s ", b);
 		else if (i % 3 == 0)
-			printf("%s", f);
+			printf("%s ", f);
 		else
-			printf("%d", i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
