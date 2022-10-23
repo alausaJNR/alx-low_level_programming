206b38f689fbd2e6d202418e308190952c250b22
alausaJNR <rasheedhabib400@gmail.com>
2022-10-23 07:04:09 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - This shows whether a given input is a digit or not
+ * @c: input
+ * Return: 1 if positive and 0 if otherwise
+ */
+int _isdigit(int c);
+{
+	if ((c <= 9) && (c >= 0))
+		return (1);
+	else
+		return (0);
+}
